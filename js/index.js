1619044300a65dfd7dc88e91a9bc497cb4133370
169398 <kulubiidris@gmail.com>
2024-04-11 08:59:36 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,105 @@
+var jsCompanyMessages = [{ "id": 7, "GUID": "F253D78C-10C1-4B4A-9B2E-7A095FA6B75A", "UID": 7667, "CID": 1518, "Message": "Hey, Load New Safeguard policies", "DTCreated": "2023-08-17T17:55:03.793", "Active": true }, { "id": 6, "GUID": "044B445F-34BC-470A-BEE4-15F3CE25C02B", "UID": 7667, "CID": 1518, "Message": "New Templates for licenses are available. Select Instructions from Dynamic Reports", "DTCreated": "2023-08-04T12:16:47.533", "Active": true }];
+var jsCalendar = [{ "slctDay": 13, "slctMonth": 3, "slctYear": 2024, "Company": "Allgate Financial, LLC", "ID": -5603, "strItemID": "-5603", "Event": "Idaho Quarterly Report - Q1.DynamicCMSFile", "Event_Date": "2024-03-13T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": true }, { "slctDay": 14, "slctMonth": 3, "slctYear": 2024, "Company": "CPC", "ID": -7105, "strItemID": "-7105", "Event": "CT_20231231.DynamicCMSFile", "Event_Date": "2024-03-14T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": false }, { "slctDay": 15, "slctMonth": 3, "slctYear": 2024, "Company": "Allgate Financial, LLC", "ID": 52600, "strItemID": "52600", "Event": "California Annual Report 20240315.docx", "Event_Date": "2024-03-15T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": true }, { "slctDay": 20, "slctMonth": 3, "slctYear": 2024, "Company": "CPC", "ID": -7107, "strItemID": "-7107", "Event": "ILLINOIS_20231231.DynamicCMSFile", "Event_Date": "2024-03-20T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": false }, { "slctDay": 21, "slctMonth": 3, "slctYear": 2024, "Company": "CPC", "ID": -7114, "strItemID": "-7114", "Event": "NEVADA_20231231.DynamicCMSFile", "Event_Date": "2024-03-21T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": false }, { "slctDay": 29, "slctMonth": 3, "slctYear": 2024, "Company": "Allgate Financial, LLC", "ID": -5607, "strItemID": "-5607", "Event": "Washington Quarterly Report - Q1.DynamicCMSFile", "Event_Date": "2024-03-29T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": true }, { "slctDay": 31, "slctMonth": 3, "slctYear": 2024, "Company": "Allgate Financial, LLC", "ID": -5590, "strItemID": "-5590", "Event": "Connecticut Annual Report.DynamicCMSFile", "Event_Date": "2024-03-31T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": true }, { "slctDay": 31, "slctMonth": 3, "slctYear": 2024, "Company": "Allgate Financial, LLC", "ID": 52419, "strItemID": "52419", "Event": "Evanston Business License_03312024 [1].pdf", "Event_Date": "2024-03-31T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": true }, { "slctDay": 31, "slctMonth": 3, "slctYear": 2024, "Company": "Capital Accounts", "ID": -6346, "strItemID": "-6346", "Event": "MA_20240331.DynamicCMSFile", "Event_Date": "2024-03-31T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": false }, { "slctDay": 31, "slctMonth": 3, "slctYear": 2024, "Company": "Scribe", "ID": -6307, "strItemID": "-6307", "Event": "CYBER_20240331.DynamicCMSFile", "Event_Date": "2024-03-31T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": false }, { "slctDay": 1, "slctMonth": 4, "slctYear": 2024, "Company": "Allgate Financial, LLC", "ID": -5597, "strItemID": "-5597", "Event": "Tennessee Annual Report.DynamicCMSFile", "Event_Date": "2024-04-01T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": true }, { "slctDay": 1, "slctMonth": 4, "slctYear": 2024, "Company": "Capital Accounts", "ID": -6398, "strItemID": "-6398", "Event": "IL_20240401.DynamicCMSFile", "Event_Date": "2024-04-01T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": false }, { "slctDay": 1, "slctMonth": 4, "slctYear": 2024, "Company": "Scribe", "ID": -6306, "strItemID": "-6306", "Event": "E&O_20240401.DynamicCMSFile", "Event_Date": "2024-04-01T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": false }, { "slctDay": 4, "slctMonth": 4, "slctYear": 2024, "Company": "CPC", "ID": -7117, "strItemID": "-7117", "Event": "NC_20231231.DynamicCMSFile", "Event_Date": "2024-04-04T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": false }, { "slctDay": 5, "slctMonth": 4, "slctYear": 2024, "Company": "CPC", "ID": -7118, "strItemID": "-7118", "Event": "ND_20231231.DynamicCMSFile", "Event_Date": "2024-04-05T00:00:00", "Event_Type": "File Expiring", "Assignments": "", "Internal": true, "Recurrence": "", "RecurrenceDays": "", "EndDate": null, "isMine": false }];
+var jsFileEvents = [{ "Box": "Files", "Type": "Outside File Remediation", "FormID": "1B15D034-7038-461A-BDFD-AF1C2934332B", "Cnt": 12 }, { "Box": "Files", "Type": "Outside Files Expired", "FormID": null, "Cnt": 74 }, { "Box": "Files", "Type": "Outside Files Expiring", "FormID": null, "Cnt": 27 }];
+var jsApplicationEvents = [{ "Box": "Applications", "Type": "Open ALLGATE Complaints", "FormID": "AE1101B5-16A7-433B-A84C-31D3AE9EF86F", "Cnt": 11 }, { "Box": "Applications", "Type": "Open Allgate-ARC Complaints and Disputes", "FormID": "BC80D4F5-D6B5-4DAD-BB63-8369FC8AC54F", "Cnt": 1 }]
+
+function StartMe() {
+	    //  header element
+    const header = document.createElement('header');
+    header.classList.add('header');
+
+    //  menu div
+    const menuDiv = document.createElement('div');
+    menuDiv.classList.add('menu');
+    menuDiv.innerHTML = '<img id="menu" src="icons/menu.png" alt="menu">';
+    header.appendChild(menuDiv);
+
+    //  logo div
+    const logoDiv = document.createElement('div');
+    logoDiv.classList.add('logo');
+    logoDiv.innerHTML = '<img id="logo" src="images/gearbox.png" alt="logo">';
+    header.appendChild(logoDiv);
+
+    //  user-info div
+    const userInfoDiv = document.createElement('div');
+    userInfoDiv.classList.add('user-info');
+    header.appendChild(userInfoDiv);
+
+    //  notification icon
+    const notificationIcon = document.createElement('img');
+    notificationIcon.id = 'notificationIcon';
+    notificationIcon.src = 'icons/notifications.png';
+    notificationIcon.alt = 'Notification Icon';
+    header.appendChild(notificationIcon);
+
+    document.body.appendChild(header);
+
+    //  body elements
+    const bodyDiv = document.createElement('div');
+    bodyDiv.classList.add('body');
+
+    //  hr element
+    const hr = document.createElement('hr');
+    hr.id = 'hr';
+    bodyDiv.appendChild(hr);
+
+    //  navigation element
+    const navigationNav = document.createElement('nav');
+    navigationNav.classList.add('navigation');
+    navigationNav.innerHTML = `<ul>
+        <li><a href="#"><img id="icons" src="icons/logout.png" alt="Logout Icon"> Log Out</a></li>
+		<li><a href="#"><img id="icons" src="icons/help.png" alt="Logout Icon">Help</a></li>
+        <li><a href="#"><img id="icons" src="icons/settings.png" alt="Logout Icon">System Administration</a></li>
+        <li><a href="#"><img id="icons" src="icons/calender.png" alt="Logout Icon">Calendar Events </a></li>
+        <li><a href="#"><img id="icons" src="icons/access.png" alt="Logout Icon">Recent Company Messages</a></li>
+        <li><a href="#"><img id="icons" src="icons/mail.png" alt="Logout Icon">Mailbox</a></li>
+        <li><a href="#"><img id="icons" src="icons/files.png" alt="Logout Icon">Dynamic Reports</a></li>
+        <li><a href="#"><img id="icons" src="icons/calender.png" alt="Logout Icon">Calendar</a></li>
+        <hr>
+        <li><a href="#"><img id="icons" src="icons/network.png" alt="Logout Icon">View Network</a></li>
+        <hr>
+        <li><a href="#"><img id="icons" src="icons/dashboard.png" alt="Logout Icon">Dashboard</a></li>
+        <li><a href="#"><img id="icons" src="icons/documents.png" alt="Logout Icon">Our Documents</a></li>
+        <li><a href="#"><img id="icons" src="icons/third.png" alt="Logout Icon">3rd Party Management</a></li>
+        <li><a href="#"><img id="icons" src="icons/application.png" alt="Logout Icon">Form Applications</a></li>
+    </ul>`;
+    bodyDiv.appendChild(navigationNav);
+
+    //  card element
+    const cardDiv = document.createElement('div');
+    cardDiv.id = 'card';
+    cardDiv.innerHTML = `<p>Good afternoon, John!</p>
+    <br>
+    <br>
+    <div>
+        What would you like to do today?
+        <select>
+            <option value=""></option>
+            <option value="viewCalendar">View Calendar</option>
+            <!-- Rest of the options -->
+        </select>
+    </div>`;
+    bodyDiv.appendChild(cardDiv);
+
+    document.body.appendChild(bodyDiv);
+
+    //  main element
+    const main = document.createElement('main');
+    main.classList.add('main');
+
+    // inbox section
+    const inboxSection = document.createElement('section');
+    inboxSection.id = 'inbox';
+    inboxSection.classList.add('content-section');
+    inboxSection.innerHTML = '<h2>Inbox</h2>';
+    main.appendChild(inboxSection);
+
+    //  calendar section
+    const calendarSection = document.createElement('section');
+    calendarSection.id = 'calendar';
+    calendarSection.classList.add('content-section');
+    calendarSection.innerHTML = '<h2>Calendar</h2>';
+    main.appendChild(calendarSection);
+
+    document.body.appendChild(main);
+}
